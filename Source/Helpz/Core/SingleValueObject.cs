3e54fea6991dfbfb43b1aa9bcda6ffa34e10a6f1
Rasmus Mikkelsen <r@smus.nu>
2015-09-16 19:54:39 +0200

    Added automated database creation

---
+++ Source/Helpz/Core/SingleValueObject.cs
@@ -0,0 +1,71 @@
+﻿// The MIT License (MIT)
+//
+// Copyright (c) 2015 Rasmus Mikkelsen
+// https://github.com/rasmus/Helpz
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+
+namespace Helpz.Core
+{
+    public abstract class SingleValueObject<T> : ValueObject, IComparable
+        where T : IComparable, IComparable<T>
+    {
+        protected SingleValueObject(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; }
+
+        public int CompareTo(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            var other = obj as SingleValueObject<T>;
+            if (other == null)
+            {
+                throw new ArgumentException($"Cannot compare '{GetType().Name}' and '{obj.GetType().Name}'");
+            }
+
+            return Value.CompareTo(other.Value);
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+
+        public override string ToString()
+        {
+            return ReferenceEquals(Value, null)
+                ? string.Empty
+                : Value.ToString();
+        }
+
+        public object GetValue()
+        {
+            return Value;
+        }
+    }
+}
